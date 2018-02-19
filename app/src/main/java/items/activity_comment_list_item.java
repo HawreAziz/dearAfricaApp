6065d5c0fc971e9e6f47e128b7ba9341af701deb
Hawre Aziz <Hawre.Aziz@hiq.se>
2018-02-19 23:28:54 +0100

    change the view of the topic and section layout, i also added apk to java/test/debug. From now on every time I commit changes I also add the apk.

--- app/src/main/java/items/activity_comment_list_item.java
+++ app/src/main/java/items/activity_comment_list_item.java
@@ -1,7 +1,9 @@
-package com.whatsup.hawre.whatsup;
+package items;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.whatsup.hawre.whatsup.R;
 
 public class activity_comment_list_item extends AppCompatActivity {
 
